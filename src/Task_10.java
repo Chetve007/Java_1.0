f00ebd947d8b2e7a52bff6dd25cdb7dd615099de
Aleksey <chetve007@mail.ru>
2018-12-30 20:03:39 +0300

    task 10

---
+++ src/Task_10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task_10 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter any line: ");
+
+        String line = scanner.nextLine();
+
+        System.out.println(line);
+    }
+}
